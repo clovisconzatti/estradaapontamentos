92daf3a25c189c507f1c03a694e536870d188afa
Claudemir <claudemir@plannersolucoes.com.br>
2023-07-26 12:35:54 -0300

    teste

--- config/database.php
+++ config/database.php
@@ -93,11 +93,11 @@ return [
         'oracle' => [
             'driver'   => 'oracle',
             'tns'      => env('DB_TNS', ''),
-            'host'     => env('DB_HOST', ''),
-            'port'     => env('DB_PORT', '1521'),
-            'database' => env('DB_DATABASE', ''),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
+            'host'     => env('DB_HOST_FOCCO', ''),
+            'port'     => env('DB_PORT_FOCCO', '1521'),
+            'database' => env('DB_DATABASE_FOCCO', ''),
+            'username' => env('DB_USERNAME_FOCCO', ''),
+            'password' => env('DB_PASSWORD_FOCCO', ''),
             'charset'  => env('DB_CHARSET', 'AL32UTF8'),
             'prefix'   => env('DB_PREFIX', ''),
             'options'   => [
