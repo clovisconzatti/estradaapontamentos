c5b9dde2c0d1685cd58c55e95c22f18f3f39b92e
Claudemir <claudemir@plannersolucoes.com.br>
2023-08-01 07:21:02 -0300

    JobsFornecedor

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,8 +18,8 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->call(function(){
-            atualizaForencedor::dispatch();
             atualizaPessoa::dispatch();
+            atualizaForencedor::dispatch();
         })->everyMinute();
     }
 
